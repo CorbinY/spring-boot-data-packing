5ed70ef4eeae7f9014bf8e787b7e8831ed307d78
sunlong <m18330262328@163.com>
2018-08-06 14:00:18 +0800

    修改Readme

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 spring-boot-data-jpa和spring-boot-data-mongodb常见各种操作的Demo。
 - 封装了对MySQL和MongoDB操作的公共的可扩展的两个Base接口；
 - 封装了操作Redis的公共接口；
+
 基于Spring Boot 1.5.9和JDK1.8，希望能够帮到你。如果帮到你了，请给我一个星星。
 ## 项目技术点
 * spring-boot-starter-data-jpa
--- src/main/java/com/lilian/JpaDemoApplication.java
+++ src/main/java/com/lilian/JpaDemoApplication.java
@@ -3,6 +3,9 @@ package com.lilian;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @Author 孙龙
+ */
 @SpringBootApplication
 public class JpaDemoApplication {
 
